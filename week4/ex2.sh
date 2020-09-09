be65a69595375b1eb08ed4c4cead5bec264a1ef2
dademd <62954763+dademd@users.noreply.github.com>
2020-09-09 11:46:39 +0300

    Update ex2.sh

--- week4/ex2.sh
+++ week4/ex2.sh
@@ -1,7 +1,9 @@
 clang -std=c11 -o ex2 ex2.c
 
 ./ex2 &
-# pstree sometimes shows all 32 processes that should be created in ex2. This is because line with pstree is executed erlier than some processes created in ex2. After ex2 sleep ends, pstree shows only one parent process ex2
+# pstree only sometimes shows all 32 processes that should be created in ex2. 
+# This is because the line with pstree is sometimes executed erlier than some processes created in ex2. 
+# After ex2 sleep ends, pstree shows only one parent process ex2
 for i in {1..4};
   do pstree; sleep 1;
 done
