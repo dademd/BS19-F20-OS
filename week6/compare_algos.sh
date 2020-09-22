efccfcf460eb0b65b4290b119f05f41f3a0058fa
dademd <d.danko@innopolis.university>
2020-09-22 21:20:36 +0000

    added details

--- week6/compare_algos.sh
+++ week6/compare_algos.sh
@@ -9,3 +9,5 @@ bash ex3.sh
 
 # Run this script to get the results of running three algorithms on the same input(for RR quanta are provided additionally)
 # Usually, SJN outperforms both RR and FCFS. SJN might be on par with RR in AVG.WT on small tests, however.
+# FCFS doesn't do any optimizations, that is why it's WT and AT are usually the greatest among three algorithms
+# RR can work like FCFS except for a bit faster if quanta are bigger than any BT, since it uses sort by AT, then by BT(just implementation-specific). It can, however, be significantly faster for some choices of quanta
